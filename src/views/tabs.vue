8127c2ba4e8e721d333b3008197e521dbe57af55
Eliseo Murillo <95434225+eliseomurillo9@users.noreply.github.com>
2023-10-03 15:56:25 +0200

    icons created and dynamic nav bar initiated (#2)
    * icons created and dynamic nav bar initiated
    
    * navbar created

--- src/views/tabs.vue
+++ src/views/tabs.vue
@@ -2,35 +2,74 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/add-event">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Ajouter un evenement</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab2" href="/tabs/search">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Search</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/home">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Accueil</ion-label>
-        </ion-tab-button>
-         <ion-tab-button tab="tab4" href="/tabs/message">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Message</ion-label>
-        </ion-tab-button>
-         <ion-tab-button tab="tab5" href="/tabs/account">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Compte</ion-label>
+      <ion-tab-bar slot="bottom" class="icon-container">
+        <ion-tab-button
+          v-for="tab in navBar"
+          :tab="tab.tabNumber"
+          :href="tab.link"
+        >
+          <ion-icon class="icon-color" aria-hidden="true" :icon="tab.icon" />
+          <ion-label style="white-space: pre-line;">{{ tab.title }}</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+<style>
+.icon-container {
+  display: flex;
+  flex-direction: row;
+}
+</style>
+<script setup>
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
+import { ref } from "vue";
+import add from "../icons/add.svg";
+import chat from "../icons/chat.svg";
+import home from "../icons/home.svg";
+import profile from "../icons/profile.svg";
+import search from "../icons/search.svg";
+
+const navBar = ref([
+  {
+    title: "Ajouter un evenement",
+    icon: add,
+    link: "/tabs/add-event",
+    tabNumber: "tab1",
+  },
+  {
+    title: "Parcourir",
+    icon: search,
+    link: "/tabs/search",
+    tabNumber: "tab2",
+  },
+  {
+    title: "Accueil",
+    icon: home,
+    link: "/tabs/home",
+    tabNumber: "tab3",
+  },
+  {
+    title: "Conversation",
+    icon: chat,
+    link: "/tabs/message",
+    tabNumber: "tab4",
+  },
+  {
+    title: "Compte",
+    icon: profile,
+    link: "/tabs/account",
+    tabNumber: "tab5",
+  },
+]);
 </script>
