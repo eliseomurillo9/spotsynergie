3e497da32f625075732767ffe749f788fac19d5f
Eliseo Murillo <eliseomurillo9@gmail.com>
2023-10-03 14:54:53 +0200

    home page started

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,22 +1,82 @@
 <template>
-      <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>HOME</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">HOME</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <h1>HELLO IM HOME</h1>
-    </ion-content>
+  <ion-page>
+   <section class="page-container">
+    <div class="header-container">
+        <div>
+            <span>ICON</span><span>Lille</span>
+        </div>
+        <div>
+            <span>{{counter}}</span>
+            <span>Selectionner une date</span>
+        </div>
+    </div>
+    <div class="img-container">
+        <img src="@/assets/tour_Eiffel.jpeg" alt="">
+    </div>
+    <button class="btn-container" @click="swipeFunction(true)"> X </button>
+    <button class="btn-container-yes" @click="swipeFunction(false)"> check </button>
+   </section>
   </ion-page>
-    
 </template>
 
+<style>
+.page-containe{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.header-container{
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem;
+}
+
+.img-container {
+    margin: auto;
+}
+
+.img-container img{
+    object-fit: cover;
+    width: 25rem;
+    height: 30rem;
+    margin: auto;
+    align-self: center ;
+}
+
+.btn-container {
+    background-color: red;
+    border-radius: 100%;
+    padding: 2rem;
+    font-size: 3rem;
+}
+
+.btn-container-yes {
+    background-color: #03DAC6;
+     border-radius: 100%;
+    padding: 2rem;
+    font-size: 3rem;
+}
+</style>
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { ref, reactive } from 'vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+
+
+const counter = ref('COUOCU');
+const list = reactive({})
+
+const swipeFunction = (response:any) => {
+    if (response) {
+        console.log(counter.value);
+    } else {
+        console.log('I DONT WANNA GO');
+    }
+}
 </script>
