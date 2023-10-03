b3235a93add508d8f941f242136691d69461574d
Eliseo Murillo <eliseomurillo9@gmail.com>
2023-10-03 14:05:27 +0200

    icons created and dynamic nav bar initiated

--- src/views/tabs.vue
+++ src/views/tabs.vue
@@ -17,11 +17,11 @@
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Accueil</ion-label>
         </ion-tab-button>
-         <ion-tab-button tab="tab4" href="/tabs/message">
+        <ion-tab-button tab="tab4" href="/tabs/message">
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Message</ion-label>
         </ion-tab-button>
-         <ion-tab-button tab="tab5" href="/tabs/account">
+        <ion-tab-button tab="tab5" href="/tabs/account">
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Compte</ion-label>
         </ion-tab-button>
@@ -30,7 +30,54 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+<script setup>
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
+import { ref } from "vue";
+import add from "../icons/add.svg";
+import chat from "../icons/chat.svg";
+import home from "../icons/home.svg";
+import profile from "../icons/profile.svg";
+import search from "../icons/search.svg";
+
+const navBar = ref([
+  {
+    title: "Ajouter un evenement",
+    icon: add,
+    link: "/tabs/add-event",
+    tabNumber: "tab1",
+  },
+  {
+    title: "Parcourir",
+    icon: search,
+    link: "/tabs/search",
+    tabNumber: "tab2",
+  },
+  {
+    title: "Accueil",
+    icon: home,
+    link: "/tabs/home",
+    tabNumber: "tab3",
+  },
+  {
+    title: "Ajouter un evenement",
+    icon: add,
+    link: "/tabs/add-event",
+    tabNumber: "tab1",
+  },
+  {
+    title: "Ajouter un evenement",
+    icon: add,
+    link: "/tabs/add-event",
+    tabNumber: "tab1",
+  },
+]);
 </script>
