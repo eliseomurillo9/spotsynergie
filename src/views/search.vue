077478687bd82e159803b1d65045f6877fb5f9ae
akeroz <arthurmery88@gmail.com>
2023-10-04 14:50:05 +0200

    ajout des cards + scroll X / Y

--- src/views/search.vue
+++ src/views/search.vue
@@ -2,6 +2,87 @@
   <ion-page>
     <Header />
     <Searchbar />
+    <section class="subject1">
+      <h1>Musées</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="subject2">
+      <h1>Cinéma</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="subject3">
+      <h1>Restaurants</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
   </ion-page>
 </template>
 
@@ -13,30 +94,58 @@ import Searchbar from '@/components/Searchbar.vue';
 
 </script>
 <style>
-  .header-container{
-    display: flex;
-    justify-content: space-between;
-    padding: 2rem;
-  }
+.ion-page{
+  overflow-y: auto;
+}
 
-  .search-bar {
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    margin: 10px;
-  }
+.header-container{
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+}
 
-  .search-bar input {
-    flex: 1;
+h1{
+  margin-left: 30px;
+}
+
+.card-container {
+    display: flex;
+    overflow-x: auto; /* Permet le défilement horizontal */
+    width: 100vw; /* 100% de la largeur de la vue (viewport) */
+    min-width: 100%; /* Assure que le conteneur a une largeur minimale égale à la largeur de l'écran */
+
+}
+
+.card {
+  min-width: 200px;
+  border: 1px solid #ccc;
+  border-radius: 30px;
+  overflow: hidden;
+  margin: 10px 20px;
+
+}
+
+.card img {
+    width: 100%;
+    height: 150px;
+    object-fit: cover;
+}
+
+.card:last-child {
+    margin-right: 0;
+}
+
+.card-content {
+    padding: 20px;
+}
+
+.card-content h2 {
+    margin: 0 0 10px;
+    font-size: 24px;
+}
+
+.card-content p {
+    margin: 0;
     font-size: 16px;
-    padding: 10px;
-    outline: none;
-    border-radius: 20px;
-    display: flex;
-    margin: 10px;
-    background-color: azure;
-    color: black;
-  }
-
+}
 </style>
