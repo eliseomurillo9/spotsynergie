25a08035c538235ee1ad800474148f3c6540f717
akeroz <55154350+akeroz@users.noreply.github.com>
2023-10-05 10:32:46 +0200

    Searchpage (#10)
    * searchbar in components
    
    * searchbar in components
    
    fix conflict with develop
    
    * ajout des cards + scroll X / Y
    
    * couleur card + ajustement
    
    * modif card
    
    ---------
    
    Co-authored-by: Eliseo Murillo <eliseomurillo9@gmail.com>

--- src/views/search.vue
+++ src/views/search.vue
@@ -2,6 +2,87 @@
   <ion-page>
     <Header />
     <Searchbar />
+    <section class="subject1">
+      <h1>Musées</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="subject2">
+      <h1>Cinémas</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="subject3">
+      <h1>Restaurants</h1>
+      <div class="card-container ion-no-padding">
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card1</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card2</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+          <div class="card-content">
+              <h2>Card3</h2>
+              <p>This is a simple card example. You can put any content here.</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
   </ion-page>
 </template>
 
@@ -13,30 +94,71 @@ import Searchbar from '@/components/Searchbar.vue';
 
 </script>
 <style>
-  .header-container{
-    display: flex;
-    justify-content: space-between;
-    padding: 2rem;
-  }
+.ion-page{
+  overflow-y: auto;
+}
 
-  .search-bar {
-    border-radius: 5px;
+.header-container{
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+}
+
+h1{
+  margin-left: 30px;
+  color: #da4e4e;
+  font-weight: bold;
+
+}
+
+h2{
+  margin-left: 30px;
+  color: #03DAC6; 
+  font-weight: bold;
+}
+
+.card-container {
     display: flex;
+    overflow-x: auto; /* Permet le défilement horizontal */
+    width: 100vw; /* 100% de la largeur de la vue (viewport) */
+    min-width: 100%; /* Assure que le conteneur a une largeur minimale égale à la largeur de l'écran */
+
+}
+
+.card {
+  background-color: #202847;
+  min-width: 200px;
+  border-radius: 30px;
+  overflow: hidden;
+  margin: 10px 20px;
+
+}
+
+.card img {
+    width: 90%;
+    height: 150px;
+    border-radius: 15px;
+    margin: 10px 10px;
     align-items: center;
-    padding: 10px;
-    margin: 10px;
-  }
-
-  .search-bar input {
-    flex: 1;
-    font-size: 16px;
-    padding: 10px;
-    outline: none;
-    border-radius: 20px;
     display: flex;
-    margin: 10px;
-    background-color: azure;
-    color: black;
-  }
+    justify-content: center;
+}
 
+.card:last-child {
+    margin-right: 0;
+}
+
+.card-content {
+    padding: 10px 10px 20px 20px;
+}
+
+.card-content h2 {
+    margin: 0 0 10px;
+    font-size: 24px;
+}
+
+.card-content p {
+    margin: 0;
+    font-size: 16px;
+}
 </style>
